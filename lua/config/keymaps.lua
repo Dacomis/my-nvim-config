b1a7398e34e1b114fb54f4cbd6f528266ee789a2
Dacomis <albei.andrei@gmail.com>
2025-03-05 10:59:49 +0200

    Remove Telescope LSP

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,7 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "gd", require("telescope.builtin").lsp_definitions, { silent = true })
 
 -- Yank Relative Path to OS clipboard
 vim.keymap.set("n", "<leader>yp", function()
